c43a273d7e824ce3044fb2eedc004a90328664d6
Xavaro <adamfrederiksen@students.westonps.org>
2016-06-06 13:42:05 -0400

    Added Texbox info

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import pygame
 import sys
 import os
@@ -29,7 +26,88 @@ sideBarFontScore = pygame.font.SysFont('monospace', 25)
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Connect 4')
 surface = pygame.display.get_surface()
-temp_user = True
+user1=''
+user2=''
+
+
+class TextBox(object):
+    def __init__(self,rect,**kwargs):
+        self.rect = pg.Rect(rect)
+        self.buffer = []
+        self.final = None
+        self.rendered = None
+        self.render_rect = None
+        self.render_area = None
+        self.blink = True
+        self.blink_timer = 0.0
+        self.process_kwargs(kwargs)
+
+    def process_kwargs(self,kwargs):
+        defaults = {"id" : None,
+                    "command" : None,
+                    "active" : True,
+                    "color" : pg.Color("white"),
+                    "font_color" : pg.Color("black"),
+                    "outline_color" : pg.Color("black"),
+                    "outline_width" : 2,
+                    "active_color" : pg.Color("blue"),
+                    "font" : pg.font.Font(None, self.rect.height+4),
+                    "clear_on_enter" : False,
+                    "inactive_on_enter" : True}
+        for kwarg in kwargs:
+            if kwarg in defaults:
+                defaults[kwarg] = kwargs[kwarg]
+            else:
+                raise KeyError("InputBox accepts no keyword {}.".format(kwarg))
+        self.__dict__.update(defaults)
+
+    def get_event(self,event):
+        if event.type == pg.KEYDOWN and self.active:
+            if event.key in (pg.K_RETURN,pg.K_KP_ENTER):
+                self.execute()
+            elif event.key == pg.K_BACKSPACE:
+                if self.buffer:
+                    self.buffer.pop()
+            elif event.unicode in ACCEPTED:
+                self.buffer.append(event.unicode)
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            self.active = self.rect.collidepoint(event.pos)
+
+    def execute(self):
+        if self.command:
+            self.command(self.id,self.final)
+        self.active = not self.inactive_on_enter
+        if self.clear_on_enter:
+            self.buffer = []
+
+    def update(self):
+        new = "".join(self.buffer)
+        if new != self.final:
+            self.final = new
+            self.rendered = self.font.render(self.final, True, self.font_color)
+            self.render_rect = self.rendered.get_rect(x=self.rect.x+2,
+                                                      centery=self.rect.centery)
+            if self.render_rect.width > self.rect.width-6:
+                offset = self.render_rect.width-(self.rect.width-6)
+                self.render_area = pg.Rect(offset,0,self.rect.width-6,
+                                           self.render_rect.height)
+            else:
+                self.render_area = self.rendered.get_rect(topleft=(0,0))
+        if pg.time.get_ticks()-self.blink_timer > 200:
+            self.blink = not self.blink
+            self.blink_timer = pg.time.get_ticks()
+
+    def draw(self,surface):
+        outline_color = self.active_color if self.active else self.outline_color
+        outline = self.rect.inflate(self.outline_width*2,self.outline_width*2)
+        surface.fill(outline_color,outline)
+        surface.fill(self.color,self.rect)
+        if self.rendered:
+            surface.blit(self.rendered,self.render_rect,self.render_area)
+        if self.blink and self.active:
+            curse = self.render_area.copy()
+            curse.topleft = self.render_rect.topleft
+            surface.fill(self.font_color,(curse.right+1,curse.y,2,curse.h))
 
 
 class Grid:
@@ -154,36 +232,42 @@ def input(events):
         # else:
         #    print event
 
+
+
+############################################################################################
 grid = Grid()
+startScreen=1
 while True:
-    pygame.draw.rect(surface, bgcolour, (0, 0, width, height), 0)
-    if highlightedcol > -1:
-        pygame.draw.rect(surface, highlightcolour, (cellwidth
-                         * highlightedcol, 0, cellwidth, height), 0)
-    for i in range(1, gridnum):
-        pygame.draw.line(surface, linecolour, (0, cellheight * i),
-                         (width_adapted, cellheight * i))
-    for i in range(1, gridnum + 1):
-        pygame.draw.line(surface, linecolour, (cellwidth * i, 0),
-                         (cellwidth * i, height))
-    for x in range(0, gridnum):
-        for y in range(0, gridnum):
-            if grid.getgrid(x, y) == 1:
-                pygame.draw.ellipse(surface, red, (cellwidth * x,
-                                    cellheight * y, cellwidth,
-                                    cellheight), 0)
-            elif grid.getgrid(x, y) == 2:
-                pygame.draw.ellipse(surface, black, (cellwidth * x,
-                                    cellheight * y, cellwidth,
-                                    cellheight), 0)
-        turn = sideBarFontUserName.render("Chuck's Turn", 1, (0, 0, 0))
-        window.blit(playeronename, (width - 150, height - 800))
-        if temp_user == False:
-            sideBarFontUserName.render("Chuck's Points :", 1, (0,0, 0))
-            window.blit(playeronename, (width - 150, height - 750))
-        else:
-            sideBarFontUserName.render('Statistics are not available', 1, (0, 0, 0))
-            window.blit(playeronename, (width - 150, height - 750))
+	
+	if startScreen>3:
+		pygame.draw.rect(surface, bgcolour, (0, 0, width, height), 0)
+		if highlightedcol > -1:
+			pygame.draw.rect(surface, highlightcolour, (cellwidth
+							 * highlightedcol, 0, cellwidth, height), 0)
+		for i in range(1, gridnum):
+			pygame.draw.line(surface, linecolour, (0, cellheight * i),
+							 (width_adapted, cellheight * i))
+		for i in range(1, gridnum + 1):
+			pygame.draw.line(surface, linecolour, (cellwidth * i, 0),
+							 (cellwidth * i, height))
+		for x in range(0, gridnum):
+			for y in range(0, gridnum):
+				if grid.getgrid(x, y) == 1:
+					pygame.draw.ellipse(surface, red, (cellwidth * x,
+										cellheight * y, cellwidth,
+										cellheight), 0)
+				elif grid.getgrid(x, y) == 2:
+					pygame.draw.ellipse(surface, black, (cellwidth * x,
+										cellheight * y, cellwidth,
+										cellheight), 0)
+			turn = sideBarFontUserName.render("Chuck's Turn", 1, (0, 0, 0))
+			window.blit(playeronename, (width - 150, height - 800))
+			if temp_user == False:
+				sideBarFontUserName.render("Chuck's Points :", 1, (0,0, 0))
+				window.blit(playeronename, (width - 150, height - 750))
+			else:
+				sideBarFontUserName.render('Statistics are not available', 1, (0, 0, 0))
+				window.blit(playeronename, (width - 150, height - 750))
 
     # playeronenamescore = sideBarFontScore.render("23812", 1, (0, 0, 0))
     # window.blit(playeronenamescore, (width-150, height-750))
@@ -191,5 +275,3 @@ while True:
     pygame.display.flip()
     if not victory:
         input(pygame.event.get())
-
-			
